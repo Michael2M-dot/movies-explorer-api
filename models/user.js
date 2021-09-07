1cc1816cba56a854de7ee01a4f3788f853fb4771
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-09-07 16:53:02 +0800

    feat:added static method findUserByCredential to userSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const AuthErr = require('../errors/authError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,5 +28,23 @@ const userSchema = new mongoose.Schema({
     select: false, // запрещаем возвращать пароль при передаче данных пользователя
   },
 }, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new AuthErr('Неверный email или пароль!'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new AuthErr('Неверный email или пароль!'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
