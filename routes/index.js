3bb5705d9a27443241b90a5267f8e076185d16b9
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-09-09 13:55:19 +0800

    feat: added not Founf Err for unexisted routes to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const NotFoundErr = require('../errors/notFoundError');
 
 router.use('/', userRouter);
 router.use('/', movieRouter);
+router.use((req, res, next) => {
+  next(new NotFoundErr('Запрашиваемый ресурс не найден'));
+});
 
 module.exports = router;
