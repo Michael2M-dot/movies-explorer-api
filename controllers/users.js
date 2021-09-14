f2c7d30acc85f885beea5f78ab5fe6ecc18d726f
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-09-14 17:36:08 +0800

    fix: rewrite secret-key

--- controllers/users.js
+++ controllers/users.js
@@ -150,7 +150,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'secret_key',
+        NODE_ENV === 'production' ? JWT_SECRET : 'secret-key',
         { expiresIn: '7d' },
       );
 
