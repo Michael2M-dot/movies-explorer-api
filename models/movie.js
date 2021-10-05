bd49ca14b45697297a1f3b15401469a1c09b675f
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-10-05 12:37:06 +0800

    feat: deleted unused dates into movie model

--- models/movie.js
+++ models/movie.js
@@ -2,25 +2,9 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const movieSchema = new mongoose.Schema({
-  country: {
+  nameRU: {
     type: String,
-    required: [true, 'Поле "country" должно быть заполнено'],
-  },
-  director: {
-    type: String,
-    required: [true, 'Поле "director" должно быть заполнено'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Поле "duration" должно быть заполнено'],
-  },
-  year: {
-    type: String,
-    required: [true, 'Поле "year" должно быть заполнено'],
-  },
-  description: {
-    type: String,
-    required: [true, 'Поле "description" должно быть заполнено'],
+    required: [true, 'Поле "nameRU" должно быть заполнено'],
   },
   image: {
     type: String,
@@ -42,33 +26,49 @@ const movieSchema = new mongoose.Schema({
       message: 'Поле "trailer" должно быть валидным url-адресом.',
     },
   },
-  thumbnail: {
-    type: String,
-    required: [true, 'Поле "thumbnail" должно быть заполнено'],
-    validate: {
-      validator(thumbnail) {
-        return validator.isURL(thumbnail);
-      },
-      message: 'Поле "thumbnail" должно быть валидным url-адресом.',
-    },
+  duration: {
+    type: Number,
+    required: [true, 'Поле "duration" должно быть заполнено'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     required: [true, 'В поле "owner" должен быть передан id владельца карточки'],
   },
-  movieId: {
-    type: Number,
-    required: [true, 'В поле "movieId" должен быть передан id фильма от сервиса MoviesExplorer'],
-  },
-  nameRU: {
-    type: String,
-    required: [true, 'Поле "nameRU" должно быть заполнено'],
-  },
-  nameEN: {
-    type: String,
-    required: [true, 'Поле "nameEN" должно быть заполнено'],
-  },
+  // country: {
+  //   type: String,
+  //   required: [true, 'Поле "country" должно быть заполнено'],
+  // },
+  // director: {
+  //   type: String,
+  //   required: [true, 'Поле "director" должно быть заполнено'],
+  // },
+  // year: {
+  //   type: String,
+  //   required: [true, 'Поле "year" должно быть заполнено'],
+  // },
+  // description: {
+  //   type: String,
+  //   required: [true, 'Поле "description" должно быть заполнено'],
+  // },
+  // thumbnail: {
+  //   type: String,
+  //   required: [true, 'Поле "thumbnail" должно быть заполнено'],
+  //   validate: {
+  //     validator(thumbnail) {
+  //       return validator.isURL(thumbnail);
+  //     },
+  //     message: 'Поле "thumbnail" должно быть валидным url-адресом.',
+  //   },
+  // },
+  // movieId: {
+  //   type: Number,
+  //   required: [true, 'В поле "movieId" должен быть передан id фильма от сервиса MoviesExplorer'],
+  // },
+  // nameEN: {
+  //   type: String,
+  //   required: [true, 'Поле "nameEN" должно быть заполнено'],
+  // },
 });
 
 module.exports = mongoose.model('movie', movieSchema);
