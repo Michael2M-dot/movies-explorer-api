2bef73b432b0be27b098b269ade6a073d9013525
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-10-09 19:59:27 +0800

    fix: mistake before deleting movie

--- controllers/movies.js
+++ controllers/movies.js
@@ -74,7 +74,7 @@ module.exports.deleteMovie = (req, res, next) => {
   //   throw new ValidationErr(errMovieIdEmpty);
   // }
 
-  Movie.findByIdAndRemove(req.params._id)
+  Movie.findById(req.params._id)
     .orFail()
     .then((movie) => {
       if (movie.owner.toString() !== req.user._id) {
